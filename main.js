fb041c5a51992978f0a87aa198d915e2b9ecca23
mailsondias7 <mailsondias7@gmail.com>
2020-07-16 19:29:53 -0300

    Cores do menu modificadas

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-
 const selectElement = (s) => document.querySelector(s);
-const navLinks = document.querySelectorAll(".nav-link")
+const navLinks = document.querySelectorAll(".nav-link");
 
-selectElement(".burger-menu-icon").addEventListener("click", ()=>{
-    selectElement(".nav-list").classList.toggle("active");
-    selectElement(".burger-menu-icon").classList.toggle("toggle")
-})
-
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+  selectElement(".nav-list").classList.toggle("active");
+  selectElement(".burger-menu-icon").classList.toggle("toggle");
+});
 
 //Animate Links
 navLinks.forEach((link, index) => {
-    if (link.style.animation){
-        link.style.animation = ""
-    }else {
-        link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-        console.log(index/7 + 0.5);
-    }
-})
-
+  if (link.style.animation) {
+    link.style.animation = "";
+  } else {
+    link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+      index / 7 + 0.5
+    }s`;
+    console.log(index / 7 + 0.5);
+  }
+});
