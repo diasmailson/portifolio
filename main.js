1a747ef0d9fdc5ba14f9831a27e21c7c5b910503
mailsondias7 <mailsondias7@gmail.com>
2020-07-22 21:33:15 -0300

    Seção Projetos

--- main.js
+++ main.js
@@ -4,16 +4,36 @@ const navLinks = document.querySelectorAll(".nav-link");
 selectElement(".burger-menu-icon").addEventListener("click", () => {
   selectElement(".nav-list").classList.toggle("active");
   selectElement(".burger-menu-icon").classList.toggle("toggle");
-});
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+      console.log(index / 7 + 0.5);
+    }
+  })
+})
 
 //Animate Links
-navLinks.forEach((link, index) => {
-  if (link.style.animation) {
-    link.style.animation = "";
-  } else {
-    link.style.animation = `navLinkAnimate 0.5s ease forwards ${
-      index / 7 + 0.5
-    }s`;
-    console.log(index / 7 + 0.5);
-  }
-});
+
+
+navLinks.forEach(link=>{
+  link.addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger-menu-icon").classList.toggle("toggle");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+        console.log(index / 7 + 0.5);
+      }
+    })
+  })
+})
